031e7ca221461fd13a48f8d06143e8d3c0f46a96
julianroman1990 <julianroman1990@gmail.com>
2024-05-15 19:45:47 -0500

    The discounts of the owner and driver entities are created, with their respective creation and update.

---
+++ src/module/driver/dto/create.driver.dto.ts
@@ -0,0 +1,51 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateDriverDto {
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  phone: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  license: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  age: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  status: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  id_owner: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  role: string;
+
+}
