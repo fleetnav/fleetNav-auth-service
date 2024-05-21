4885769ba47bca706196537d3004333236e1c940
cxmi02 <149894510+cxmi02@users.noreply.github.com>
2024-05-20 22:40:25 -0500

    the guardian, the roles are added and the readme is complemented

--- src/libs/auth/auth.module.ts
+++ src/libs/auth/auth.module.ts
@@ -7,7 +7,7 @@ import { OwnerModule } from 'src/module/owner/owner.module';
 
 @Module({
   imports: [DriverModule, OwnerModule, JwtModule.register({
-    secret: process.env.JWT_SECRET || 'mi-semilla-secreta',
+    secret: process.env.JWT_SECRET,
     signOptions: { expiresIn: process.env.ACCESS_TOKEN_EXPIRY || '60s' }
   })],
   controllers: [AuthController],
--- src/libs/auth/controllers/auth.controller.ts
+++ src/libs/auth/controllers/auth.controller.ts
@@ -1,30 +1,39 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { LoginAuthDto } from '../dto';
 import { AuthService } from '../services/auth.service';
 import { CreateDriverDto } from 'src/module/driver/dto';
 import { CreateOwnerDto } from 'src/module/owner/dto';
+import { AtGuard } from '../guard/auth.guard';
+import { Roles } from '../../decorators/roles/roles.decorators';
+import { Role } from '../../decorators/roles/role.enum';
+import { IsPublic } from '../../decorators/public.decorator';
 
 @Controller('auth')
+@UseGuards(AtGuard)
 export class AuthController {
     
   constructor(private readonly authService: AuthService) {}
 
   @Post('register/driver')
+  @Roles(Role.Driver)
   registerDriver(@Body() createDriverDto: CreateDriverDto) {
     return this.authService.registerDriver(createDriverDto);
   }
 
   @Post('register/owner')
+  @Roles(Role.Owner)
   registerOwner(@Body() createOwnerDto: CreateOwnerDto) {
     return this.authService.registerOwner(createOwnerDto);
   }
 
   @Post('login/driver')
+  @IsPublic() // Ruta pública
   loginDriver(@Body() loginDriverDto: LoginAuthDto) {
     return this.authService.loginDriver(loginDriverDto);
   }
 
   @Post('login/owner')
+  @IsPublic() // Ruta pública
   loginOwner(@Body() loginOwnerDto: LoginAuthDto) {
     return this.authService.loginOwner(loginOwnerDto);
   }
